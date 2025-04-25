64eec9a1b6e717534de805dc5274b2ba6b32926d
negromanto <negromanto81@hotmail.com>
2025-04-25 18:25:05 +0200

    Agregar archivos de proyecto.

---
+++ Register.cs
@@ -0,0 +1,35 @@
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace websiteWatcher;
+
+public class Register(ILogger<Register> logger)
+{
+    [Function(nameof(Register))]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+        var options = new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true
+        };
+
+        Website newWebsite = JsonSerializer.Deserialize<Website>(requestBody, options);
+        newWebsite.Id = Guid.NewGuid();
+
+        //logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult(newWebsite);
+    }
+}
+
+public class Website
+{
+    public Guid Id { get; set; }
+    public string Url { get; set; }
+    public string XPathExpression { get; set; }
+
+}
